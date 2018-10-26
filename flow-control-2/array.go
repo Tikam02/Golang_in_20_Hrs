565e938b1852d197179aa6bfd5aac61d2705a782
[Tikam02] <timonalma81@gmail.com>
2018-10-26 21:48:39 +0530

    -m

--- flow-control-2/array.go
+++ flow-control-2/array.go
@@ -10,3 +10,17 @@
 // declares a variable a as an array of ten integers.
 
 // An array's length is part of its type, so arrays cannot be resized. This seems limiting, but don't worry; Go provides a convenient way of working with arrays.
+package main
+
+import "fmt"
+
+func main(){
+	var a [2]string
+	a[0] = "hello"
+	a[1] = "World"
+	fmt.Println(a[0],a[1])
+	fmt.Println(a)
+
+	prime := [6]int{2,3,5,7,11,13}
+	fmt.Println(prime)
+}
---
+++ flow-control-2/slice-bounds.go
@@ -0,0 +1,33 @@
+
+// Slice defaults
+
+// When slicing, you may omit the high or low bounds to use their defaults instead.
+
+// The default is zero for the low bound and the length of the slice for the high bound.
+
+// For the array
+
+// var a [10]int
+
+// these slice expressions are equivalent:
+
+// a[0:10]
+// a[:10]
+// a[0:]
+// a[:]
+
+package main
+import "fmt"
+
+func main(){
+	s := []int{2,4,5,6,8,5,99,7777}
+
+	s = s[1:4]
+	fmt.Println(s)
+
+	s = s[:2]
+	fmt.Println(s)
+
+	s = s[1:]
+	fmt.Println(s)
+}
